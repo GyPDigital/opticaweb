5453aa27b69f0a3b91facc3353bf19f931ed0a9f
German <german.casta.rojas@gmail.com>
2022-07-15 16:49:49 -0500

    Finalizacion navbar para moviles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,25 @@ import "../assets/css/Navbar.css";
 
 function Navbar (){
     return(
-        <div className="navbar">
-            <img className="navbar-logo" src={require("../assets/imgs/logo-optica-template.png")} alt="Logo optica" />
+        <div className="navbar" id="nav">
+            <div className="navbar-imgs">
+                <img className="navbar-logo" src={require("../assets/imgs/logo-optica-template.png")} alt="Logo optica" />
+                <a href="#" className="navbar-close">
+                    <img src={require("../assets/icons/equis.png")} alt="Icono equis cerrar" />
+                </a>
+                <a href="#nav" className="navbar-hamburguer">
+                    <img  src={require("../assets/icons/menu.png")} alt="Icono hamburguesa" />
+                </a>
+                <div className="divider-reverse">
+                    <hr className="shine-reverse"/>
+                </div>
+            </div>
+            
             <div className="navbar-link-box">
-              	<a href="www.gypdigital.com"><img src={require("../assets/icons/home.png")} alt="Icono home" /> Inicio</a>
-				<a href="www.gypdigital.com"><img src={require("../assets/icons/glasses.png")} alt="Icono productos gafas" /> Productos</a>
-                <a href="www.gypdigital.com"><img src={require("../assets/icons/contact.png")} alt="Icono telefono contacto" /> Contacto</a>
-                <a href="www.gypdigital.com"><img src={require("../assets/icons/about-us.png")} alt="Icono personas nosotros" /> Nosotros</a>
+              	<a href="www.gypdigital.com">Inicio</a>
+				<a href="www.gypdigital.com">Productos</a>
+                <a href="www.gypdigital.com">Nosotros</a>
+                <a href="www.gypdigital.com">Contacto</a>
             </div>
         </div>
     );
