a2ec155606390e6520f749a39b5ff9ebbe0f87a8
German <german.casta.rojas@gmail.com>
2022-07-15 18:45:48 -0500

    Actualizacion de navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Navbar.css";
 
-function Navbar (){
+function Navbar(){
+    /* despliega u oculta el menú */
+    const [deploy, setDeploy] = useState(false);
+
     return(
         <div className="navbar" id="nav">
             <div className="navbar-imgs">
                 <img className="navbar-logo" src={require("../assets/imgs/logo-optica-template.png")} alt="Logo optica" />
-                <a href="#" className="navbar-close">
+                <button className={!deploy ? "navbar-cls btn-hide" : "navbar-cls btn-show"} onClick={() => setDeploy(!deploy)}>
                     <img src={require("../assets/icons/equis.png")} alt="Icono equis cerrar" />
-                </a>
-                <a href="#nav" className="navbar-hamburguer">
+                </button>
+                <button className={deploy ? "navbar-ham btn-hide" : "navbar-ham btn-show" } onClick={() => setDeploy(!deploy)}>
                     <img  src={require("../assets/icons/menu.png")} alt="Icono hamburguesa" />
-                </a>
+                </button>
                 <div className="divider-reverse">
                     <hr className="shine-reverse"/>
                 </div>
             </div>
-            
-            <div className="navbar-link-box">
+            <div className={ deploy ? "navbar-link-box opened-links" : "navbar-link-box closed-links" }>
               	<a href="www.gypdigital.com">Inicio</a>
 				<a href="www.gypdigital.com">Productos</a>
                 <a href="www.gypdigital.com">Nosotros</a>
