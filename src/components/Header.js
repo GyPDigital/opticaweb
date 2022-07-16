0764309c3ca9d839d076a7a15c85a218c9955bf8
German <german.casta.rojas@gmail.com>
2022-07-16 18:42:25 -0500

    Actualizacion componenete Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Header.css";
+import headersData from "./data/data-header";
 
 function Header(){
+
+    const [imgToShow, setImgToShow] = useState(0);
+
+    function handleImg(n) {
+
+        // seccion que restablece el index para que nunca sea mayor o menor al numero de imagenes
+        if(n > headersData.length-1){
+            setImgToShow(0)
+        }else if(n < 0){
+            setImgToShow(headersData.length-1)
+        }else{
+            setImgToShow(n);
+        }
+
+        console.log("cantidad de imgs en array : "+headersData.length);
+        console.log("imagen a mostrar : "+imgToShow);
+    }
+
     return(
-        <div></div>
+        <div className="header-container">
+            {
+                headersData.map( (e, i) => 
+                    <div
+                        key={e.img+i} 
+                        className="header-img"
+                        style={{
+                            backgroundImage: "url("+require("../assets/imgs/header/"+e.img)+")",
+                            display: imgToShow === i ? "block" : "none"
+                        }}
+                    />
+                )
+            }
+            <button className="btn-header btn-r" onClick={()=>{ handleImg( imgToShow + 1 ) }} />
+            <button className="btn-header btn-l" onClick={()=>{ handleImg( imgToShow - 1 ) }} />
+            <div className="list-pagination">
+                {
+                    headersData.map((e, i) => 
+                        <span 
+                            key={"dot"+i}
+                            className="dot"
+                            style={{ opacity: imgToShow === i ? "1" : "0.5"}}
+                        />
+                    )
+                }
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/data/data-header.js
@@ -0,0 +1,20 @@
+const headers = [
+    {
+        img: "chica-con-lentes.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel ante tortor. Aliquam rhoncus felis nibh, in interdum libero tincidunt at. Integer convallis tincidunt augue."
+    },
+    {
+        img: "hombre-con-lentes-en-tienda.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel ante tortor. Aliquam rhoncus felis nibh, in interdum libero tincidunt at. Integer convallis tincidunt augue."
+    },
+    {
+        img: "hombre-con-lentes.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel ante tortor. Aliquam rhoncus felis nibh, in interdum libero tincidunt at. Integer convallis tincidunt augue."
+    },
+    {
+        img: "lentes-tienda.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel ante tortor. Aliquam rhoncus felis nibh, in interdum libero tincidunt at. Integer convallis tincidunt augue."
+    }
+];
+
+export default headers;
