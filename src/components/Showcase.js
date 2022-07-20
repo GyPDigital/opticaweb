cf2a9c8b5098fb047798be58b5fb0bf16956e71b
German <german.casta.rojas@gmail.com>
2022-07-20 18:41:50 -0500

    Actualizacion Showcase

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,65 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/Showcase.css";
+import products from "./data/data-products";
 
 function Showcase(){
+
+    const [category, setCategory] = useState("offer");
+
+    const [catList, setCatList] = useState([]);
+
+    function handleCategory(cat){
+
+        if(cat === "offer" || cat === "luxory" || cat === "new" || cat === "cheap"){
+            setCategory(cat);
+        }else{
+            alert("No existe la categoria seleccionada");
+        }
+    }
+
+    useEffect(()=>{
+        setCatList( products.filter(item => item.category === category) );
+    },[category]);
+
     return(
         <div className="container">
             <div className="showcase">
                 <div className="sc-title">
                     <h5>Vitrina Virtual</h5>
-                    <h1>Nuestros Lentes</h1>
-                    <p>Lo mejor de nosotros para ti</p>
+                    <h1>Nuestros Productos</h1>
+                    <p>Lo mejor en marcos y monturas para ti</p>
                 </div>
                 <div className="sc-category">
                     <h3>Categorias</h3>
                     <div className="sc-category-btn">
                         <div className="btn-wrap">
-                            <button className="round-btn categ-offer"/>
+                            <button className="round-btn categ-offer" onClick={()=>{ handleCategory("offer") }}/>
                             <h5>Oferta</h5>
                         </div>
                         <div className="btn-wrap">
-                            <button className="round-btn categ-cheap"/>
+                            <button className="round-btn categ-cheap" onClick={()=>{ handleCategory("cheap") }}/>
                             <h5>Economico</h5>
                         </div>
                         <div className="btn-wrap">
-                            <button className="round-btn categ-luxury"/>
+                            <button className="round-btn categ-luxury" onClick={()=>{ handleCategory("luxory") }}/>
                             <h5>Lujo</h5>
                         </div>
                         <div className="btn-wrap">
-                            <button className="round-btn categ-new"/>
+                            <button className="round-btn categ-new" onClick={()=>{ handleCategory("new") }}/>
                             <h5>Nuevo</h5>
                         </div>
                     </div>
                 </div>
                 <div className="sc-items-box">
-                    <div className="item">
-                        <img src={require("../assets/imgs/products/rounded-black.jpg")} alt="gafas tipo redondeadas negras" />
-                        <div className="item-text">
-                            <h5>Redondeado negro</h5>
-                            <p><strong>$30.000</strong></p>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <img src={require("../assets/imgs/products/transparent-lennon.jpg")} alt="gafas tipo lenon transparentes" />
-                        <div className="item-text">
-                            <h5>Lennon transparentes</h5>
-                            <p><strong>$40.000</strong></p>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <img src={require("../assets/imgs/products/rounded-black.jpg")} alt="gafas tipo redondeadas negras" />
-                        <div className="item-text">
-                            <h5>Redondeado negro</h5>
-                            <p><strong>$30.000</strong></p>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <img src={require("../assets/imgs/products/transparent-lennon.jpg")} alt="gafas tipo lenon transparentes" />
-                        <div className="item-text">
-                            <h5>Lennon transparentes</h5>
-                            <p><strong>$40.000</strong></p>
-                        </div>
-                    </div>
+                    {
+                        catList.map( (prod, i) =>
+                            <div className="item" key={"item-"+i}>
+                                <img src={require("../assets/imgs/products/"+prod.img)} alt={prod.alt} />
+                                <div className="item-text">
+                                    <h5>{prod.name}</h5>
+                                    <p><strong>{prod.price}</strong></p>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>
--- src/components/data/data-products.js
+++ src/components/data/data-products.js
@@ -1,43 +1,59 @@
 const products = [
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "gold-slim.jpg",
+        alt: "Marco de gafas tipo Gold Slim",
+        name: "Montura Gold Slim",
+        price: "$120.000",
+        category: "luxory"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "rounded-black.jpg",
+        alt: "Marco de gafas tipo Rounded Black",
+        name: "Montura Rounded Back",
+        price: "$50.000",
+        category: "cheap"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "silver-classic.jpg",
+        alt: "Marco de gafas tipo Silver Classic",
+        name: "Montura Silver Classic",
+        price: "$65.000",
+        category: "offer"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "simple-black-frame.jpg",
+        alt: "Marco de gafas tipo Simple Black Frame",
+        name: "Montura Simple Black Frame",
+        price: "$75.000",
+        category: "offer"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "sport-glasses.jpg",
+        alt: "Marco de gafas tipo Sport",
+        name: "Montura Deportiva",
+        price: "$80.000",
+        category: "luxory"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "sun-glasses.jpg",
+        alt: "Marco de gafas tipo Gafas de Sol",
+        name: "Montura Gafas de Sol",
+        price: "$100.000",
+        category: "new"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "transparent-lennon.jpg",
+        alt: "Marco de gafas tipo Lennon Transparentes",
+        name: "Montura Lennon Transparente",
+        price: "$180.000",
+        category: "luxory"
     },
     {
-        img: ".jpg",
-        name: "",
-        price: "$"
+        img: "trendy-red.jpg",
+        alt: "Marco de gafas tipo Trendy Red",
+        name: "Montura Trendy Red",
+        price: "$80.000",
+        category: "new"
     }
 ]
 
