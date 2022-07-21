b5593f7d65accf9b6973b1319556203cfadcf697
German <german.casta.rojas@gmail.com>
2022-07-21 16:59:06 -0500

    Actualizacion Componente Showcase

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,22 +3,31 @@ import "../assets/css/Showcase.css";
 import products from "./data/data-products";
 
 function Showcase(){
-
+    // categoria a mostrar
     const [category, setCategory] = useState("offer");
-
+    // catList lista de productos filtrados por la categoria seleccionada
     const [catList, setCatList] = useState([]);
+    // preloader muestra una animacion de carga, no hay necesidad pero se ve chevere XD
+    const [preloader, setPreloader] = useState(false);
 
     function handleCategory(cat){
 
-        if(cat === "offer" || cat === "luxory" || cat === "new" || cat === "cheap"){
-            setCategory(cat);
+        if(cat === "offer" || cat === "luxury" || cat === "new" || cat === "classic" || cat === "all"){
+            // se establece una animacion de 0.5s antes de cargar los nuevos items
+            setPreloader(true);
+            setTimeout(()=>{
+                setCategory(cat);
+                setPreloader(false);
+            }, 500);
+            
         }else{
             alert("No existe la categoria seleccionada");
         }
     }
 
     useEffect(()=>{
-        setCatList( products.filter(item => item.category === category) );
+        // cuando se actualiza el estado categoria, se carga la lista de items a mostrar
+        setCatList( category === "all" ? products : products.filter(item => item.category === category) );
     },[category]);
 
     return(
@@ -37,22 +46,28 @@ function Showcase(){
                             <h5>Oferta</h5>
                         </div>
                         <div className="btn-wrap">
-                            <button className="round-btn categ-cheap" onClick={()=>{ handleCategory("cheap") }}/>
-                            <h5>Economico</h5>
+                            <button className="round-btn categ-cheap" onClick={()=>{ handleCategory("classic") }}/>
+                            <h5>Clasico</h5>
                         </div>
                         <div className="btn-wrap">
-                            <button className="round-btn categ-luxury" onClick={()=>{ handleCategory("luxory") }}/>
+                            <button className="round-btn categ-luxury" onClick={()=>{ handleCategory("luxury") }}/>
                             <h5>Lujo</h5>
                         </div>
                         <div className="btn-wrap">
                             <button className="round-btn categ-new" onClick={()=>{ handleCategory("new") }}/>
                             <h5>Nuevo</h5>
                         </div>
+                        <div className="btn-wrap">
+                            <button className="round-btn categ-all" onClick={()=>{ handleCategory("all") }}/>
+                            <h5>Todos</h5>
+                        </div>
                     </div>
                 </div>
                 <div className="sc-items-box">
                     {
-                        catList.map( (prod, i) =>
+                        preloader // carga el preloader entes de mostrar los items
+                        ? <div className="loader"/>
+                        : catList.map( (prod, i) =>
                             <div className="item" key={"item-"+i}>
                                 <img src={require("../assets/imgs/products/"+prod.img)} alt={prod.alt} />
                                 <div className="item-text">
--- src/components/data/data-products.js
+++ src/components/data/data-products.js
@@ -4,21 +4,21 @@ const products = [
         alt: "Marco de gafas tipo Gold Slim",
         name: "Montura Gold Slim",
         price: "$120.000",
-        category: "luxory"
+        category: "luxury"
     },
     {
         img: "rounded-black.jpg",
         alt: "Marco de gafas tipo Rounded Black",
         name: "Montura Rounded Back",
         price: "$50.000",
-        category: "cheap"
+        category: "classic"
     },
     {
         img: "silver-classic.jpg",
         alt: "Marco de gafas tipo Silver Classic",
         name: "Montura Silver Classic",
         price: "$65.000",
-        category: "offer"
+        category: "classic"
     },
     {
         img: "simple-black-frame.jpg",
@@ -30,14 +30,14 @@ const products = [
     {
         img: "sport-glasses.jpg",
         alt: "Marco de gafas tipo Sport",
-        name: "Montura Deportiva",
+        name: "Montura Deportiva Run Master",
         price: "$80.000",
-        category: "luxory"
+        category: "luxury"
     },
     {
         img: "sun-glasses.jpg",
         alt: "Marco de gafas tipo Gafas de Sol",
-        name: "Montura Gafas de Sol",
+        name: "Gafas de Sol Azul Iridiscente",
         price: "$100.000",
         category: "new"
     },
@@ -46,13 +46,83 @@ const products = [
         alt: "Marco de gafas tipo Lennon Transparentes",
         name: "Montura Lennon Transparente",
         price: "$180.000",
-        category: "luxory"
+        category: "luxury"
     },
     {
         img: "trendy-red.jpg",
         alt: "Marco de gafas tipo Trendy Red",
         name: "Montura Trendy Red",
         price: "$80.000",
+        category: "offer"
+    },
+    {
+        img: "sport-v400-cycling.jpg",
+        alt: "Gafas deportivas v400 cycling",
+        name: "Gafas deportivas v400 cycling",
+        price: "$120.000",
+        category: "new"
+    },
+    {
+        img: "scicon-aero-shade.jpg",
+        alt: "Gafas Deportivas Scicon Aero Shade",
+        name: "Gafas Deportivas Sc-Icon Aero Shade",
+        price: "$100.000",
+        category: "luxury"
+    },
+    {
+        img: "polarized-c05-cycling.jpg",
+        alt: "Gafas Deportivas polarized C05 Cycling",
+        name: "Gafas Deportivas polarized C05 Cycling",
+        price: "$110.000",
+        category: "new"
+    },
+    {
+        img: "pepa-pig.jpg",
+        alt: "Marco Pepa Pig",
+        name: "Marco Pepa Pig Rosa",
+        price: "$50.000",
+        category: "offer"
+    },
+    {
+        img: "hypercraft-blue.jpg",
+        alt: "Gafas Hypercraft Blue",
+        name: "Gafas Deportivas Hypercraft Blue",
+        price: "$200.000",
+        category: "luxury"
+    },
+    {
+        img: "hypercraft-iridescent.jpg",
+        alt: "Gafas Hypercraft Iridescent",
+        name: "Gafas Deportivas Hypercraft Iridescent",
+        price: "$250.000",
+        category: "luxury"
+    },
+    {
+        img: "dama-moderna.jpg",
+        alt: "Marco dama diseño moderno",
+        name: "Marco para dama diseño moderno",
+        price: "$85.000",
+        category: "offer"
+    },
+    {
+        img: "marco-vahnissi.jpg",
+        alt: "Marco vahnissi",
+        name: "Marco vahnissi",
+        price: "$65.000",
+        category: "classic"
+    },
+    {
+        img: "orbital-fade-blue.jpg",
+        alt: "Marco orbital fade blue",
+        name: "Marco Orbital Fade Blue",
+        price: "$70.000",
+        category: "classic"
+    },
+    {
+        img: "aviator-xe800-gold.jpg",
+        alt: "Marco aviator xe800 gold",
+        name: "Marco Aviator XE-800 Gold",
+        price: "$270.000",
         category: "new"
     }
 ]
